44344937cd357b27859bcd87b0e50e582829875c
NavajazosLokos <orochivenom123@gmail.com>
2026-02-06 22:28:29 -0600

    Se añadio IRouters y schemas

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
+import models
+
+from routers.items import router as items_router
 
 app = FastAPI()
 
-# Crear tablas si no existen
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Crear tablas
 Base.metadata.create_all(bind=engine)
+
+# Registrar routers
+app.include_router(items_router)
 
 @app.get("/")
 def read_root():
